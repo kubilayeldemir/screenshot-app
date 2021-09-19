ae38518c3346f98c5829950883ef5781fd2e495f
kubilayeldemir <kubi0707@gmail.com>
2021-09-19 23:55:31 +0300

    AShot demo: take screenshot

--- screenshot-engine/src/main/java/com/prtsc/screenshotengine/ScreenshotEngineApplication.java
+++ screenshot-engine/src/main/java/com/prtsc/screenshotengine/ScreenshotEngineApplication.java
@@ -1,13 +1,35 @@
 package com.prtsc.screenshotengine;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ru.yandex.qatools.ashot.AShot;
+import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
+
+import javax.imageio.ImageIO;
+import java.io.File;
 
 @SpringBootApplication
 public class ScreenshotEngineApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ScreenshotEngineApplication.class, args);
+		SpringApplication.run(ScreenshotEngineApplication.class, args).close();
+		System.setProperty("webdriver.chrome.driver", "C:\\Projects\\screenshot-app\\screenshot-engine\\chromedriver.exe");
+
+		WebDriver webDriver = new ChromeDriver();
+		webDriver.get("https://www.youtube.com/watch?v=V4NJo2Mfvrc");
+		var screenshot = new AShot()
+				.shootingStrategy(ShootingStrategies.viewportPasting(100))
+				.takeScreenshot(webDriver);
+		try{
+			ImageIO.write(screenshot.getImage(),"PNG",new File("./Screenshots/elementScreenshot.png"));
+		}
+		catch (Exception e){
+			System.out.println(e);
+		}
+		webDriver.quit();
+
 	}
 
 }
--- screenshot-engine/src/test/java/com/prtsc/screenshotengine/ScreenshotEngineApplicationTests.java
+++ screenshot-engine/src/test/java/com/prtsc/screenshotengine/ScreenshotEngineApplicationTests.java
@@ -1,6 +1,8 @@
 package com.prtsc.screenshotengine;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -9,5 +11,11 @@ class ScreenshotEngineApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
+	@Test
+	void driverTest(){
+		System.setProperty("webdriver.chrome.driver", "C:\\Projects\\screenshot-app\\screenshot-engine\\chromedriver.exe");
+		WebDriver webDriver = new ChromeDriver();
+		webDriver.get("https://www.youtube.com");
+		webDriver.quit();
+	}
 }
