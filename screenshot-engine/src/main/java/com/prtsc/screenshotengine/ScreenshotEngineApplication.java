d6f778a80ddf1b6aaa4b8a2f60ce8fc49a394d5b
kubilayeldemir <kubi0707@gmail.com>
2021-09-19 23:20:07 +0300

    initial commit

---
+++ screenshot-engine/src/main/java/com/prtsc/screenshotengine/ScreenshotEngineApplication.java
@@ -0,0 +1,13 @@
+package com.prtsc.screenshotengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenshotEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenshotEngineApplication.class, args);
+	}
+
+}
---
+++ screenshot-engine/src/test/java/com/prtsc/screenshotengine/ScreenshotEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prtsc.screenshotengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenshotEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
